50f3ca9d0120f9b10ccf5b10b90198e3bd7e942e
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2022-11-10 06:05:13 +0900

    add gnuplot option (with line)

--- graphene_e/save.sh
+++ graphene_e/save.sh
@@ -1,18 +1,33 @@
 
 CMDNAME=`basename $0`
 
-while getopts lo: OPT
+while getopts li:o: OPT
 do
   case $OPT in
+    "i" ) FLG_IN=1 ; IN_NAME="$OPTARG" ;;
     "l" ) FLG_LINE=1 ;;
-    "o" ) FLG_TITLE=1 ; FILE_NAME="$OPTARG" ;;
+    "o" ) FLG_OUT=1 ; OUT_NAME="$OPTARG" ;;
       * ) echo "Usage: $CMDNAME [-l] [-o VALUE]" 1>&2
           exit 1 ;;
   esac
 done
 
-if [ ! -z "$FLG_LINE" ]; then
-    
-else then
 
+if [ -z "$FLG_IN" ]; then
+    IN_NAME="out.txt"
 fi
+
+if [ -z "$FLG_OUT" ]; then
+    OUT_NAME="out.png"
+fi
+
+if [ ! -z "$FLG_LINE" ]; then
+    OPTION="with lines"
+fi
+
+gnuplot << EOF
+set terminal png
+set output "$IN_NAME"
+plot $OUT_NAME OPTION
+exit
+EOF
