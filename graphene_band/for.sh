ab65d3537184accf09990bb23f498da6fce1e1f6
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2022-12-15 20:49:43 +0900

    delete

--- graphene_band/for.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pw.x < graphene.scf.in > graphene.scf.out
-
-pw.x < graphene.nscf.in > graphene.nscf.out
-
-bands.x < graphene.band.in > graphene.band.out
--- graphene_dos/for.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pw.x < graphene.scf.in > graphene.scf.out
-
-pw.x < graphene.nscf.in > graphene.nscf.out
-
-dos.x < graphene.dos.in > graphene.dos.out
